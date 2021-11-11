c9121f81e8b2410ca87e4b371d5f8bf76a304426
unknown <alexm1397@gmail.com>
2021-11-10 18:17:49 -0600

    some base files

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+
+
+-- table for dept: id int primary key, name varchar(30)
+-- table for role: id int primary key, title varchar(30), salary decimal, dept_id int to hold reference to dept
+-- table for employees: id int primary key, firstname varchar(30) lastname varchar(30), role id for reference to role, manager id referneces managers
