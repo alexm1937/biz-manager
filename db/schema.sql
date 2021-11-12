da32bc3f35b6aad6ef9f4dbd1e2a617990b999a4
unknown <alexm1397@gmail.com>
2021-11-11 18:39:44 -0600

    db and tables created & seeded with examples;

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,29 @@
+DROP DATABASE IF EXISTS biz;
+CREATE DATABASE biz;
+USE biz;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
 
--- table for dept: id int primary key, name varchar(30)
--- table for role: id int primary key, title varchar(30), salary decimal, dept_id int to hold reference to dept
--- table for employees: id int primary key, firstname varchar(30) lastname varchar(30), role id for reference to role, manager id referneces managers
+CREATE TABLE department (
+    dept_id INTEGER PRIMARY KEY, 
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    role_id INTEGER PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,3) NOT NULL,
+    dept_id INTEGER,
+    CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(dept_id) ON DELETE SET NULL
+); 
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER
+    -- manager_id VARCHAR(30) SELECT 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO department (dept_id, dept_name)
+VALUES
+(1, 'Engineering'),
+(2, 'Accounting'),
+(3, 'Grunts');
+
+INSERT INTO roles (role_id, role_title, salary, dept_id)
+VALUES
+(1, 'Lead Engineer', 100000.75, 1),
+(2, 'Senior Engineer', 75199.00, 1),
+(3, 'Junior Engineer', 45000.00, 1),
+(4, 'Lead Accountant', 100000.75, 2),
+(5, 'Accountant Teamate', 75000.00, 2),
+(6, 'General Labor', 39500.00, 3);
+
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES 
+(1, 'alex', 'mcdaniel', 1),
+(2, 'joe', 'butler', 5);
+
+
