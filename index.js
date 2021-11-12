afb76054eb811116920ed01506bff67a5e3e0bf7
unknown <alexm1397@gmail.com>
2021-11-12 14:26:40 -0600

    database connection created using mysql2, index connects to db and queries db tables

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-//require: inquirer
+
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+//imports connection to DB
+const db = require('./db/connection.js')
+
 //define sql query actions and import here from queries.sql
 
 //requirer prompts here
 
+var startApp = function() {
+    db.query(
+        `SELECT * FROM department`,
+        function(err, results, fields) {
+            console.log(results);
+            console.log(fields);
+        }
+    )
+}
+
+db.connect(err=> {
+    if(err) throw err;
+    console.log('Database connected');
+});
+
+startApp();
+    
