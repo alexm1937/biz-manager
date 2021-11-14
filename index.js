e8d158ba1d951f77acb995ed4a0683c31dcd1370
unknown <alexm1397@gmail.com>
2021-11-13 20:04:30 -0600

    consolidated back into index to allow callback to work

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 //imports connection to DB
 const db = require('./db/connection.js')
-//import functions
-const { addDept, viewAllDept } = require('./utils/dept');
-const { addEmployee } = require('./utils/employees.js');
-const { addPosition } = require('./utils/positions.js');
+
 
 db.connect(err=> {
     if(err) throw err;
@@ -26,7 +23,14 @@ const setPrompt = () => {
     })
     .then(({nextPrompt}) => {
         if(nextPrompt === 'View all Departments') {
-            viewAllDept();
+            db.query(
+                `SELECT * FROM department`,
+                function(err, results) {
+                    if(err) throw err;
+                    console.table(results);
+                    setPrompt();
+                }
+            )
         };
         if(nextPrompt === 'View all Positions') {
             db.query(
@@ -49,14 +53,105 @@ const setPrompt = () => {
             )
         };
         if(nextPrompt === 'Add a Department') {
-            addDept();
-            // calling setPrompt here also introduces errors and interrupts prompts
+            inquirer.prompt({
+                type: 'text',
+                name: 'deptName',
+                message: 'What is the name of the new Department?'
+            })
+            .then(({deptName}) => {
+                db.query(
+                    `INSERT INTO department (dept_name)
+                    VALUES
+                    ('${deptName}');`,
+                    function(err, results) {
+                        if(err) throw err;
+                        console.log('Department successfully created!');
+                        // calling setPrompt here returns error 'Is not function'
+                    }
+                )
+            })
         }
         if(nextPrompt === 'Add an Employee') {
-            addEmployee();
+            var getId = function(position) {
+                let positionId;
+                if(position === 'Lead Engineer') {
+                    return positionId = 1; 
+                }
+            }
+            inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'first_name',
+                    message: 'What is the First Name of your new Employee?'
+                },
+                {
+                    type: 'text',
+                    name: 'last_name',
+                    message: 'What is the Last Name of your new Employee?'
+                },
+                {
+                    type: 'list',
+                    name: 'position',
+                    message: 'What is the Position of your new Employee?',
+                    choices: ['Lead Engineer', 'Senior Engineer', 'Junior Engineer', 'Lead Accountant', 'Accountant Teamate', 'General Labor']
+                }
+                // {
+                //     type: 'text',
+                //     name: 'manager',
+                //     message: 'Who is the manager in charge of your new Employee?'
+                // }
+            ])
+            .then(({ first_name, last_name, position }) => {
+                db.query(
+                    `INSERT INTO employees (first_name, last_name, position)
+                    VALUES 
+                    ('${first_name}', '${last_name}', '${getId(position)}');
+                    `,
+                    function(err, results) {
+                        if(err) throw err;
+                        console.log('Employee successfully created!');
+                    }
+                )
+            })
         }
         if(nextPrompt === 'Add a Position') {
-            addPosition();
+            var getId = function(department) {
+                let departmentId;
+                if(department === 'Engineering') {
+                    return departmentId = 1; 
+                }
+            }
+            inquirer.prompt([
+                // propmts for: role name, salary, and department
+                {
+                    type: 'text',
+                    name: 'position_title',
+                    message: 'Please enter a title for this new position'
+                },
+                {
+                    type: 'number',
+                    name: 'salary',
+                    message: 'Please enter the salary (without commas)'
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Please select which department this position belongs under',
+                    choices: ['Engineering', 'Accounting', 'Grunts']
+                }
+            ])
+            .then(({ position_title, salary, department }) => {
+                db.query(
+                    `INSERT INTO positions (position_title, salary, dept_id)
+                    VALUES 
+                    ('${position_title}', '${salary}', '${getId(department)}');
+                    `,
+                    function(err, results) {
+                        if(err) throw err;
+                        console.log('Position successfully created!');
+                    }
+                )
+            })
         }
         if(nextPrompt === 'exit') {
             return process.abort();
