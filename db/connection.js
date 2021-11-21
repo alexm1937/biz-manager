3b3282dc3133aa6a31ca826873476b23fe74f8eb
unknown <alexm1397@gmail.com>
2021-11-21 12:10:32 -0600

    removed local mysql info from connection.js, added instruction comments and updated readme

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,8 @@ const db = mysql.createConnection({
     //Note to anyone installing locally: Must have mysql installed! 
     // Replace user and password text inside '' with your own 
     // username and password!
-    user: 'root',
-    password: 'BradIsRad1!',
+    user: 'enter_your_mysql_local_username',
+    password: 'enter_your_mysql_password',
     database: 'biz'
     }, 
     console.log('Connected to biz database')
